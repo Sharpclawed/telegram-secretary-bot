e975a5209178f8319bce5cd94a821282c8115726
albychev_dv <albychev_dv@skbkontur.ru>
2020-04-05 13:55:03 +0500

    пока отключил автоматические миграции

--- TelegramBotTry1/Migrations/Configuration.cs
+++ TelegramBotTry1/Migrations/Configuration.cs
@@ -6,7 +6,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
             ContextKey = "TelegramBotTry1.MsgContext";
         }
 
